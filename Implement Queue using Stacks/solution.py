82fcde5b181a98d9f7fcc91f04d71eadb42763c9
Mahak01299 <mahak01299@gmail.com>
2026-04-23 11:37:34 +0530

    Update solution.py

--- Implement Queue using Stacks/solution.py
+++ Implement Queue using Stacks/solution.py
@@ -1,14 +1,24 @@
-class Solution:
-    def isValid(self, s):
-        stack = []
-        mapping = {')': '(', '}': '{', ']': '['}
-        
-        for char in s:
-            if char in mapping:
-                top = stack.pop() if stack else '#'
-                if mapping[char] != top:
-                    return False
-            else:
-                stack.append(char)
-        
-        return len(stack) == 0
+class MyQueue:
+
+    def __init__(self):
+        self.stack1 = []  # for push
+        self.stack2 = []  # for pop/peek
+
+    def push(self, x):
+        self.stack1.append(x)
+
+    def pop(self):
+        self._transfer()
+        return self.stack2.pop()
+
+    def peek(self):
+        self._transfer()
+        return self.stack2[-1]
+
+    def empty(self):
+        return not self.stack1 and not self.stack2
+
+    def _transfer(self):
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
